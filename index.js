eb8f9df29cab2982406a9fbafa6b4b0ea037d8b6
Dominik Sysojew-Osinski <u0001@mmaps.org>
2022-05-27 23:59:13 +0100

    separated tags and commits

--- index.js
+++ index.js
@@ -50,15 +50,18 @@ async function run() {
 
     core.debug(await execFile('git', ['commit', '-m', `Bumped up versions to ${version}`]));
 
+    core.debug(`pushing commits`);
+    core.debug(await execFile('git', ['push']));
+
     const tag_git = core.getInput('git_create_tag');
     core.debug(`git_create_tag is set to ${tag_git}`);
     if(tag_git === true){
       core.debug(`creating tag ${version}`);
       core.debug(await execFile('git', ['tag', version, '-m', version]));
+      core.debug(await execFile('git', ['push', 'origin', version]));
     }
     
-    core.debug(`pushing commits`);
-    core.debug(await execFile('git', ['push', '--all']));
+    
 
   } catch (error) {
     core.setFailed(error.message);
