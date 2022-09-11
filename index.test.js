8e61598c1459229c49c95b13993f0deebc25d325
Dominik Sysojew-Osinski <sysojewosinskidominik@gmail.com>
2022-09-11 11:50:44 +0100

    Made tests runnable in parallel

--- index.test.js
+++ index.test.js
@@ -3,76 +3,82 @@ const cp = require('child_process');
 const path = require('path');
 const os = require('os');
 const fs = require('fs');
+const crypto = require('crypto');
 
 const { run } = require("./index");
 
-const testDir = path.join(os.tmpdir(), "dotnet-deployment-versioning-test");
-
 test('update csproj without push', async () => {
-  createCleanTestRepository();
+  const myTestDir = createCleanTestRepository();
 
   process.env['INPUT_AUTO_PUSH'] = 'false';
   process.env['GITHUB_REF'] = 'test';
 
-  await runBumpup();
+  await runBumpup(myTestDir);
 
   const status = cp.execSync('git status', {
-    cwd: testDir
+    cwd: myTestDir
   }).toString();
 
   expect(status).toContain("Your branch is ahead of");
   expect(status).toContain("by 1 commit");
 
   const author = cp.execSync('git log -1 --pretty=format:"%an <%ae>"', {
-    cwd: testDir
+    cwd: myTestDir
   }).toString();
 
-  expect(author).toBe("dotnet-deployment-versioning <actions@users.noreply.github.com>")
+  expect(author).toBe("dotnet-deployment-versioning <actions@users.noreply.github.com>");
 });
 
 test('update csproj on detached head', async () => {
 
-  createCleanTestRepository();
+  const myTestDir = createCleanTestRepository();
 
   process.env['INPUT_AUTO_PUSH'] = 'false';
   process.env['GITHUB_REF'] = 'test';
 
   cp.execSync('git checkout 1d27974c187b5d87ad1a0e9202818db8c45f5c7e', {
-    cwd: testDir
+    cwd: myTestDir
   });
 
-  await runBumpup();
+  await runBumpup(myTestDir);
 
   const status = cp.execSync('git status', {
-    cwd: testDir
+    cwd: myTestDir
   }).toString();
 
   expect(status).toContain("HEAD detached at 1d27974");
   expect(status).toContain("nothing to commit, working tree clean");
 });
 
-async function runBumpup() {
+
+
+async function runBumpup(myTestDir) {
   oldCwd = process.cwd();
-  process.chdir(testDir);
+  process.chdir(myTestDir);
   await run();
   process.chdir(oldCwd);
 }
 
 function createCleanTestRepository() {
-  if (fs.existsSync(testDir)) {
-    fs.rmSync(testDir, {
-      recursive: true
-    });
-  }
-  cp.execSync('git clone https://github.com/vakus/dotnet-deployment-versioning-test.git', {
-    cwd: os.tmpdir()
+  const testDir = path.join(os.tmpdir(), "dotnet-deployment-versioning-test");
+
+  let myTestDir = testDir + "-" + crypto.randomUUID().toString();
+
+  fs.cpSync(testDir, myTestDir, {
+    recursive: true
   });
+
+  return myTestDir;
 }
 
 afterAll(() => {
-  if (fs.existsSync(testDir)) {
-    fs.rmSync(testDir, {
-      recursive: true
-    });
-  }
+  fs.readdirSync(os.tmpdir())
+    .filter(name => name.startsWith("dotnet-deployment-versioning-test"))
+    .map(f => fs.rmSync(path.join(os.tmpdir(), f), {recursive: true}))
 });
+
+beforeAll(() => {
+  cp.execSync('git clone https://github.com/vakus/dotnet-deployment-versioning-test.git', {
+    cwd: os.tmpdir()
+  });
+})
